bb8f900bfaf2f02a51fdf6562b6dd16e38149bd4
Shlomi Asraf <148898079+shlomiasraf@users.noreply.github.com>
2025-03-21 13:43:14 +0200

    update

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.oneclick.travel',
+  appName: 'OneClickTravel',
+  webDir: 'dist'
+};
+
+export default config;
